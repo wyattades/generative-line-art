80e16e50ab94561ce71fa94f3e8112cf7fe9d1cc
Wyatt Ades <wyattades@gmail.com>
2018-04-19 02:40:01 -0700

    Added more styles, started line generation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# generative-line-art
+# Generative Line Art
+
+[Live demo](https://wyattades.github.com/generative-line-art)
+
+A single-page website whose purpose is to programmatically generate line art based off of certain variables. Specifically, the graphics are constructed from an array of lines flowing in the same direction, but with various curves that mimic some 3D surface.
+
+The main inspiration for this project was the line art of Moritz Resl, Martin Grödl, Karl Crull, and Anders Hoff, who all have had similar concepts, but vary wildly in style.
--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+// Enable Two.js for ESLint and JSDoc (for VSCode)
+/* global Two */
+/// <reference path="./lib/two.d.js" />
+
+/*
+  Helper functions
+*/
+const rand = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+
+/*
+  Setup DOM interactions
+*/
 
 const $container = document.getElementById('root');
+const $controls = document.getElementById('controls');
+
+const control = (key, onChange) => {
+  const $el = $controls.elements[key];
+  if (!$el) throw new Error('Invalid control key: ' + key);
+
+  $el.addEventListener('change', onChange);
+  return () => {
+    $el.removeEventListener('change', onChange);
+  };
+};
+
+/*
+  Setup two.js
+*/
 
 const params = {
   width: $container.offsetWidth,
@@ -7,11 +36,38 @@ const params = {
 };
 const two = new Two(params).appendTo($container);
 
-window.onresize = () => {
+// Resize canvas when window resizes and DOM fully loads
+const resize = () => {
+  two.width = $container.offsetWidth;
+  two.height = $container.offsetHeight;
   two.renderer.setSize($container.offsetWidth, $container.offsetHeight);
+};
+window.onresize = resize;
+window.onload = resize;
+
+/*
+  Render shapes
+*/
+
+const lines = two.makeGroup();
+lines.translation.set(100, 100);
+for (let i = 0; i < 20; i++) { // add 20 lines
+  const line = two.makeCurve(0, i * 22, true);
+  lines.add(line);
 }
+lines.noFill();
 
-const rect = two.makeRectangle(100, 50, 120, 30);
-rect.fill = 'rgb(0, 200, 255)';
+let iter = 0;
+two.bind('update', (frameCount) => { // run for 100 frames
+  if (iter++ > 100) two.pause();
 
-two.update();
+  for (const line of lines.children) {
+    const v = new Two.Vector(iter * 5, line.vertices[line.vertices.length-1].y + rand(-5, 5));
+    line.vertices.push(v);
+  }
+}).play();
+
+control('color', (e) => {
+  lines.stroke = e.target.value;
+  two.update();
+});
